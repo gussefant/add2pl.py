b034f90c67e4e71f827c3eae4c51c91107ad75b4
Karl August Hoivik <gussefant@gmail.com>
2014-10-20 21:33:46 +0200

    Cleaned up and added comments

--- add2pl.py
+++ add2pl.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
-#Script for adding songs to nyxmms2 playlist
+#Script for adding songs to nyxmms2 playlist using fzf
 import os
 import subprocess
-user_finder = subprocess.Popen("whoami", stdout=subprocess.PIPE, shell=True)
-(user, err) = user_finder.communicate()
-music_dir = "/home/" + user.decode("utf-8").rstrip('\n') + "/Music"
-playlist_file = music_dir + "/playlist"
-fzf = "/home/" + user.decode("utf-8").rstrip('\n') + "/.fzf/fzf"
-playlist = open(playlist_file, 'r')
+user_finder = subprocess.Popen("whoami", stdout=subprocess.PIPE, shell=True)    # Get the username
+(user, err) = user_finder.communicate()                                         # for future use
+
+music_dir = "/home/" + user.decode("utf-8").rstrip('\n') + "/Music" # The directory the script searches through. Default is "~/Music"
+playlist_file = music_dir + "/playlist" # An intermediary file where the songs will be put prior to adding them to the playlist
+fzf = "/home/" + user.decode("utf-8").rstrip('\n') + "/.fzf/fzf"    # The full path for junegunn's "fzf"-program
+
+# Command that the script will run to allow the user pick desired songs. Finds all .mp3's
+# in music_dir, sorts them, runs them through fzf and puts the choices in playlist_file.
 song_chooser = "find "+music_dir+" -iname '*.mp3' | sort | " +fzf + " +s -e -m -0 > " + playlist_file
-os.system(song_chooser)
-for song in playlist:
-    add_command = 'nyxmms2 add "' +song.rstrip('\n')+ '"'
-    os.system(add_command)
-playlist.close()
+
+os.system(song_chooser) # Run command above
+playlist = open(playlist_file, 'r') # Open playlist_file for reading
+for song in playlist: # Loop through chosen songs...
+    add_command = 'nyxmms2 add "' +song.rstrip('\n')+ '"' # ...construct a command for adding the song to the playlist...
+    os.system(add_command) # ...and run the command
+playlist.close() # Close playlist as we have finished reading it.
